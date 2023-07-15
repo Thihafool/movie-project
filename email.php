c4e8c1315648557dc3efd45fa3f0cc0cdf0b4a5c
LAPTOP-4JDKDQ2Q\User <thihafool2002@gmail.com>
2023-07-15 07:06:47 +0630

    this is test

--- email.php
+++ email.php
@@ -1,13 +1,10 @@
 <?php
 echo "sending emal";
 
-echo "another email"
-echo "third line from cli"
-
 echo "another email";
 echo "third line from online";
 
-echo "forth line from cli"
+echo "forth line from cli";
 
 	echo "titi tr tr";
 
@@ -16,7 +13,7 @@ echo "fourth line from online";
 
 echo "fifth line from online";
 
-echo "fifth line from cli"
+echo "fifth line from cli";
 
-
+echo "this is testing";
 ?>
